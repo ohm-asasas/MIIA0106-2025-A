064005784a3ec32ab40e994eab6971e62f9092ef
ohm-asasas <6811130044@mut.ac.th>
2025-11-25 17:14:40 +0700

    อัพเซิฟ

---
+++ Lab2/lad2/lad2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Hello LAB2!\n";
+
+    std::cout << "- Name: Ongart" << std::endl;
+    std::cout << "- Conrse: Miia0106" << std::endl;
+    std::cout << "- Email: 6811130044@mut.ac.th" << std::endl;
+    return 0;
+}
